1d2c096423f4e29f4b32bc7d841a920805b85f98
Blaise NDUWIMANA <nduwimanablaise@gmail.com>
2025-01-27 11:29:57 +0200

    Add contribute and browse to the menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,16 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('words.create')" :active="request()->routeIs('words.create')">
+                        {{ __('Contribute') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('words.index')" :active="request()->routeIs('words.index')">
+                        {{ __('Browse') }}
+                    </x-nav-link>
+                </div>
                 
             </div>
 
@@ -78,6 +88,17 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('words.create')" :active="request()->routeIs('words.create')">
+                {{ __('Contibute') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('words.index')" :active="request()->routeIs('words.index')">
+                {{ __('Browse') }}
             </x-responsive-nav-link>
         </div>
 
