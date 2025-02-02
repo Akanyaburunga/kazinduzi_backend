061aaf4a3af4c8ec3d513c892a9bf29be05b6c5d
Blaise NDUWIMANA <nduwimanablaise@gmail.com>
2025-02-03 00:29:42 +0200

    Custom logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('images/logo_kazinduzi_abstract.PNG') }}" alt="My Logo" class="h-12">
                     </a>
                 </div>
 
