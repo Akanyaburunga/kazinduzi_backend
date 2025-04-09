4543098dfa527764244e21d3f77dc138b9432a7e
vadoniz <155906247+vadoniz@users.noreply.github.com>
2025-04-09 14:12:48 +0200

    Update app.blade.php
    Ikirundi gikeya.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="@yield('meta-description', 'Kazinduzi ni urubuga')">
+        <meta name="description" content="@yield('meta-description', 'Kazinduzi ni urubuga rwo kwiyibutsa amajambo n'insiguro yayo mu Kirundi')">
 
         <!-- Open Graph -->
         <meta property="og:title" content="@yield('title', 'Kazinduzi')" />
