4d94b29a5653edbcb41dc22e6e1c346050cc4e33
Blaise NDUWIMANA <nduwimanablaise@gmail.com>
2025-04-10 11:46:28 +0200

    Shorten menu item

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('leaderboard.index')" :active="request()->routeIs('leaderboard.index')">
-                        {{ __('🏆 Abarusha abandi mu guterera') }}
+                        {{ __('🏆 Abahizi') }}
                     </x-nav-link>
                 </div>
                 
@@ -144,7 +144,7 @@
 
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('leaderboard.index')" :active="request()->routeIs('leaderboard.index')">
-                {{ __('🏆 Abarusha abandi mu guterera') }}
+                {{ __('🏆 Abahizi') }}
             </x-responsive-nav-link>
         </div>
 
